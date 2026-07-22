7c9bcbeff6d2304003c069e5b95e76eeccb04068
Ajay4145 <ajaytamang4145@gmail.com>
2026-07-22 10:51:50 +0545

    Add About and Features components, implement smooth scrolling, and enhance Hero section with visual cues

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/home/hero";
+import About from "@/components/home/about";
+import Features from "@/components/home/features";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
+      <Features />
     </>
   );
 }
